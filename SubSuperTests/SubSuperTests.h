483261374375ce9a5a906e6576a5463cbd8dc1b8
Irio Irineu Musskopf Junior <iirineu@gmail.com>
2013-06-22 19:08:14 -0300

    Setup testing application

---
+++ SubSuperTests/SubSuperTests.h
@@ -0,0 +1,13 @@
+//
+//  SubSuperTests.h
+//  SubSuperTests
+//
+//  Created by Irio Irineu Musskopf Junior on 22/06/13.
+//  Copyright (c) 2013 Irio Irineu Musskopf Junior. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SubSuperTests : SenTestCase
+
+@end
---
+++ SubSuperTests/SubSuperTests.m
@@ -0,0 +1,32 @@
+//
+//  SubSuperTests.m
+//  SubSuperTests
+//
+//  Created by Irio Irineu Musskopf Junior on 22/06/13.
+//  Copyright (c) 2013 Irio Irineu Musskopf Junior. All rights reserved.
+//
+
+#import "SubSuperTests.h"
+
+@implementation SubSuperTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SubSuperTests");
+}
+
+@end
